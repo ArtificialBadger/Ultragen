8a709888feb4de53ddc29dd0be7660ee6242ca1e
Isaac Zais <isaaczais@hotmail.com>
2020-02-25 07:32:46 -0800

    Moved Name file, migrated generation from linqpad file

--- Ultragen.Shell/Ultragen.Shell/Program.cs
+++ Ultragen.Shell/Ultragen.Shell/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Markov;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Ultragen.Shell
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            // Create a chain with letters as the unit of data, basing each letter on the previous 2 letters
+            var chain = new MarkovChain<char>(3);
+            //chain.
+
+            foreach (var name in await GetCityNames())
+            {
+                chain.Add(name);
+            }
+
+            //var uniqueNames = 100;
+
+            // Randomly generate words that resemble the words in the dictionary.
+            var rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var word = new string(chain.Chain(rand).ToArray());
+                Console.WriteLine(word.Trim());
+            }
+
+            Console.ReadLine();
+        }
+
+        public static async Task<IEnumerable<string>> GetCityNames()
+        {
+
+            string filePath = System.IO.Path.GetFullPath(@"..\..\..\British Names.txt");
+            var file = new StreamReader(filePath);
+
+            var rawNames = await file.ReadToEndAsync();
+
+            return rawNames
+                .Split(Environment.NewLine)
+                .Select(n => n.Trim())
+                .Select(n => n.Replace("(", ""))
+                .Select(n => n.Replace(")", ""))
+                .Select(n => n.Contains(',') ? String.Join(' ', n.Split(',').Reverse()) : n);
         }
     }
 }
