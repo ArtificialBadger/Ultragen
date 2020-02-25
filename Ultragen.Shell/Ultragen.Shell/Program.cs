e7fd02d6f55a83bda049abd18e6e9056841a0539
Isaac Zais <isaaczais@hotmail.com>
2020-02-25 07:17:56 -0800

    Added .Net Core Console Project

---
+++ Ultragen.Shell/Ultragen.Shell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ultragen.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
